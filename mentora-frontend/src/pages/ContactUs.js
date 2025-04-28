661624b218e63e0656257c4cc967e42672ea89e0
Palak <eatizsipam@gmail.com>
2025-04-28 13:36:02 +0530

    contact us updation

--- mentora-frontend/src/pages/ContactUs.js
+++ mentora-frontend/src/pages/ContactUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/Contact.css";
+import "../styles/ContactUs.css";
 import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaGlobe } from 'react-icons/fa';
 
 const Contact = () => {
